9be5a3f9601abb75f76acc3a922ce5b9f582b1e6
Jenny Cheng <jenny14789632@gmail.com>
2025-02-10 16:01:30 -0800

    add split by size test

---
+++ pdfsam-tools/pdfsam-split-by-size/src/test/java/org/pdfsam/tools/splitbysize/SplitBySizeToolTest.java
@@ -0,0 +1,37 @@
+package org.pdfsam.tools.splitbysize;
+
+
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.pdfsam.model.tool.ToolDescriptor;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class SplitBySizeToolTest {
+
+
+    private SplitBySizeTool tool;
+
+
+    @BeforeEach
+    void setUp() {
+        tool = new SplitBySizeTool();
+    }
+
+
+    @Test
+    void testId() {
+        assertEquals("split.bysize", tool.id());
+    }
+
+
+    @Test
+    void testGraphic() {
+        Node graphic = tool.graphic();
+        assertNotNull(graphic);
+    }
+}
