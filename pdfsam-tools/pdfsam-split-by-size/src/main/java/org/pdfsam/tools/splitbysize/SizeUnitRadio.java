e8f1201f9acb5b3c38144800ce98a4b79afb8742
keyanmiao <keyanm2@uci.edu>
2025-03-04 14:15:49 -0800

    testable design

--- pdfsam-tools/pdfsam-split-by-size/src/main/java/org/pdfsam/tools/splitbysize/SizeUnitRadio.java
+++ pdfsam-tools/pdfsam-split-by-size/src/main/java/org/pdfsam/tools/splitbysize/SizeUnitRadio.java
@@ -1,61 +1,65 @@
-/*
- * This file is part of the PDF Split And Merge source code
- * Created on 12/dic/2014
- * Copyright 2017 by Sober Lemur S.r.l. (info@soberlemur.com).
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.pdfsam.tools.splitbysize;
 
 import javafx.scene.control.RadioButton;
 import org.pdfsam.model.ui.workspace.RestorableView;
 
 import java.util.Map;
-import java.util.Optional;
 
 import static org.sejda.commons.util.RequireUtils.requireNotNullArg;
 
 /**
- * Radio button for {@link SizeUnit}
- * 
- * @author Andrea Vacondio
+ * Radio button for {@link SizeUnit}, refactored for better testability.
  */
 class SizeUnitRadio extends RadioButton implements RestorableView {
-
     private final SizeUnit unit;
+    private final SizeUnitRadioLogic logic;
 
     public SizeUnitRadio(SizeUnit unit) {
         requireNotNullArg(unit, "Unit cannot be null");
         this.unit = unit;
+        this.logic = new SizeUnitRadioLogic(unit);
+        initializeUI();
+    }
+
+    /** Initialize UI elements */
+    private void initializeUI() {
         this.setText(unit.friendlyName());
         this.setId("unit" + unit.symbol());
     }
 
     @Override
     public void saveStateTo(Map<String, String> data) {
-        if (isSelected()) {
-            data.put(unit.toString(), Boolean.TRUE.toString());
-        }
+        logic.saveStateTo(data, isSelected());
     }
 
     @Override
     public void restoreStateFrom(Map<String, String> data) {
-        Optional.ofNullable(data.get(unit.toString())).map(Boolean::valueOf).ifPresent(this::setSelected);
+        boolean isSelected = logic.restoreStateFrom(data);
+        this.setSelected(isSelected);
     }
 
     public SizeUnit unit() {
         return unit;
     }
+}
 
+/**
+ * Logic class for handling SizeUnitRadio state, decoupled from UI for testability.
+ */
+class SizeUnitRadioLogic {
+    private final SizeUnit unit;
+
+    public SizeUnitRadioLogic(SizeUnit unit) {
+        this.unit = unit;
+    }
+
+    public void saveStateTo(Map<String, String> data, boolean isSelected) {
+        if (isSelected) {
+            data.put(unit.toString(), Boolean.TRUE.toString());
+        }
+    }
+
+    public boolean restoreStateFrom(Map<String, String> data) {
+        return Boolean.parseBoolean(data.getOrDefault(unit.toString(), "false"));
+    }
 }
